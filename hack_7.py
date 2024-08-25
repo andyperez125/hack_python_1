5878a45b5fb97a466549ff960f4f489a55594e4b
Andy Perez <andyperez125@gmail.com>
2024-08-25 15:22:29 -0400

    ultima coreecion

--- hack_7.py
+++ hack_7.py
@@ -3,5 +3,9 @@ loop: while [] output => [5,4,3,2,1,0]
 """
 
 def fn_hack_7():
-    return [5, 4, 3, 2, 1, 0]
-
+    result = []
+    i = 5
+    while i >= 0:
+        result.append(i)
+        i -= 1
+    return result  
